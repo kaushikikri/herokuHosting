967f7a7668916d258a769ad7915a14141b7eee9b
kaushikikri <kaushikumari03@gmail.com>
2022-03-11 16:07:19 +0530

    2

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 
 const io = socketio(server);
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
